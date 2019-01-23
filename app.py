58166b7e33fbade96921190b0a7a2323b21ecd2d
รับทำแอพ รับทำ app รับทำ Mobile App <sarunsak.ph@gmail.com>
2019-01-23 11:14:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,29 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, jsonify
+from flask_restful import Api
+
 from flask_jwt import JWT, jwt_required, current_identity
 
-from security import authenticate, identity
-from db import db
-from resource.product import Product, ProductList, ProductHomeList
-from resource.banner  import BannerList
-import datetime
-import pymysql
+from db import connection
+
+# from model.amphur import amphurList
 
 app = Flask(__name__)
 api = Api(app)
-app.secret_key = 'IDMAX'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://siamtak_usr:Saim@22333@27.254.59.108:3306/siamtak_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-app.config['JWT_SECRET_KEY'] = 'secret-test'
 
-jwt = JWT(app, authenticate, identity)
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-items = []
+@app.route('/amphur')
+def home():
+     with connection.cursor() as cursor:
+     cursor.execute("SELECT * FROM tbl_amphur")
+     rv = cursor.fetchall()
+     return jsonify(rv)
 
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    parser.add_argument('salt',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
- 
-api.add_resource(BannerList, '/banners')
-api.add_resource(ProductList, '/product-lists')
-api.add_resource(ProductHomeList, '/product-home-lists')
+# api.add_resource(model.amphurList, '/amphurs')
+
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(debug=True)  # important to mention debug=True
+
+    app.run(debug=True, port=5000)  # important to mention debug=True
