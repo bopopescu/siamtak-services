8ecee8ca5ccc6e38b9fd5adb750d555a332b4710
รับทำแอพ รับทำ app รับทำ Mobile App <sarunsak.ph@gmail.com>
2019-01-23 11:51:18 +0700

    Update banner.py

--- resource/banner.py
+++ resource/banner.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required
+#from flask_jwt_extended import jwt_required
 from model.model_banner import TblBanners
 import datetime
 import json
