7bd34bec2b74bca737732ea6307fcf86a5c93675
รับทำแอพ รับทำ app รับทำ Mobile App <sarunsak.ph@gmail.com>
2019-01-23 11:43:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,48 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+from security import authenticate, identity
+from db import db
+from resource.product import Product, ProductList, ProductHomeList
+from resource.banner  import BannerList
+import datetime
+import pymysql
+
 app = Flask(__name__)
-#app.run(debug=True)
-stores = [{
-    'name': 'My Store',
-    'items': [{'name':'my item', 'price': 15.99 }]
-}]
+api = Api(app)
+app.secret_key = 'IDMAX'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://siamtak_usr:Saim@22333@27.254.59.108:3306/siamtak_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['JWT_SECRET_KEY'] = 'secret-test'
 
+jwt = JWT(app, authenticate, identity)
 
-@app.route('/')
-def index():
-    jsinData = {
-                'ddddd' : '1',
-                'ffffff' : '2',
-                }
-    return jsonify(jsinData)
+items = []
 
-@app.route('/hello')
-def hello():
-    return jsonify(stores)
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('salt',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+#
+# api.add_resource(BannerList, '/item/<string:name>')
+api.add_resource(BannerList, '/banners')
+api.add_resource(ProductList, '/product-lists')
+api.add_resource(ProductHomeList, '/product-home-lists')
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(debug=True)  # important to mention debug=True
